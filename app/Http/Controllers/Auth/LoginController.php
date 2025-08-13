ae17a6a39d9f017e1a293b3fa255d5e69e5fa96c
cricri312 <t1m4c1n0@gmail.com>
2025-08-14 01:20:17 +0200

    test

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -15,7 +15,7 @@ class LoginController extends Controller
 
     public function store(Request $request)
     {
-        Log::info('Login attempt', $request->all());
+        \Log::info('Login attempt', $request->all());
         $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required'],
