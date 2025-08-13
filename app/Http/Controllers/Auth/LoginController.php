672b8d25467779ba10912ad572f57585b4aa37bd
cricri312 <t1m4c1n0@gmail.com>
2025-08-14 01:23:32 +0200

    test

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
@@ -16,17 +17,23 @@ class LoginController extends Controller
     public function store(Request $request)
     {
         \Log::info('Login attempt', $request->all());
+
         $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required'],
         ]);
 
+        $user = User::where('email', $request->email)->first();
+        \Log::info('User found', ['user' => $user ? $user->toArray() : null]);
+
         if (!Auth::attempt($request->only('email', 'password'), $request->boolean('remember'))) {
+            \Log::info('Auth failed');
             throw ValidationException::withMessages([
                 'email' => 'Nieprawidłowy email lub hasło.',
             ]);
         }
 
+        \Log::info('Auth success, redirecting');
         $request->session()->regenerate();
 
         return redirect()->intended(route('dashboard'));
